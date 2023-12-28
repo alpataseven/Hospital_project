607380872f1815951634e50ccb5f87ac153c3a97
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2023-12-28 23:09:52 +0300

    UPDATE...

--- Hospital_project/frm_sign_up.cs
+++ Hospital_project/frm_sign_up.cs
@@ -31,7 +31,16 @@ namespace Hospital_project
         sqlConnect bgl = new sqlConnect();
         private void sign_up_Click(object sender, EventArgs e)
         {
-
+            SqlCommand komut = new SqlCommand("insert into (hasta_adi, hasta_soyadi, hasta_TC, hasta_telefon, hasta_sifre, hasta_cinsiyet) values (@p1, @p2, @p3, @p4, @p5, @p6)", bgl.baglanti("Data Source=Alp\\SQLEXPRESS;Initial Catalog=Hospital_project;Integrated Security=True;Trust Server Certificate=True"));
+            komut.Parameters.AddWithValue("@p1", txtName.Text);
+            komut.Parameters.AddWithValue("@p2", txtSurname.Text);
+            komut.Parameters.AddWithValue("@p3", txtNumber.Text);
+            komut.Parameters.AddWithValue("@p4", txtPhoneNumber.Text);
+            komut.Parameters.AddWithValue("@p5", txtPassword.Text);
+            komut.Parameters.AddWithValue("@p6", cmbSex.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Kaydınız başarıyla oluşturuldu.. Şifreniz: " + txtPassword.Text,"Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
